8ef75cbfda92918b0f3663cc5ef5876b9fbbc615
gkatari <gkatari@csc.com>
2016-05-25 12:46:41 +0530

    train

---
+++ firstApp/firstApp/Controllers/SchoolController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstApp.Controllers
+{
+    public class SchoolController : Controller
+    {
+        // GET: School
+        //public ActionResult FirstView()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        public ActionResult FirstView(int ? i)
+        {
+           
+            Response.Write("value" + i);
+            return View("FirstView");
+        }
+    }
+}
